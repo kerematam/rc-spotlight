899f4fee34bb70fc3280f10306f666830be3ffb1
Kerem ATAM <kerematam@gmail.com>
2019-12-23 23:27:25 +0300

    refactor: add example code

--- example/src/App.js
+++ example/src/App.js
@@ -6,32 +6,32 @@ import "./index.css";
 const ShadowText = text => <h1 className="shadow-text">{text}</h1>;
 
 const App = () => {
-  const [hasShadowBitMap, setHasShadowBitMap] = useState(1);
+  const [shadowIndex, setshadowIndex] = useState(0);
   useEffect(() => {
     setInterval(function() {
-      setHasShadowBitMap(hasShadowBitMap =>
-        hasShadowBitMap === 8 ? 1 : hasShadowBitMap * 2
+      setshadowIndex(shadowIndex =>
+        shadowIndex === 4 ? 1 : shadowIndex + 1
       );
-    }, 5000);
+    }, 2000);
   }, []);
 
   return (
     <div>
       <LightboxWrapper
-        isActive={1 & hasShadowBitMap}
+        isActive={1 === shadowIndex}
         renderOnShadow={ShadowText("This is title")}
       >
         <h1 className="header">Lightbox Wrapper</h1>
       </LightboxWrapper>
       <LightboxWrapper
-        isActive={2 & hasShadowBitMap}
+        isActive={2 === shadowIndex}
         renderOnShadow={ShadowText("This is subtitle")}
       >
         <h2>You can NOT touch shadowed areas</h2>
         <h2>Sibling tag: You dont need to wrap it to one div</h2>
       </LightboxWrapper>
       <LightboxWrapper
-        isActive={4 & hasShadowBitMap}
+        isActive={3 === shadowIndex}
         renderOnShadow={ShadowText("This is text input")}
       >
         <input size="large" placeholder="Type something" />
