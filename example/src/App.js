b4a543e97c18c1e9fa97113a88674768c84410cf
katam <kerematam@gmail.com>
2020-01-20 23:08:48 +0300

    feat: improve example styling

--- example/src/App.js
+++ example/src/App.js
@@ -3,54 +3,54 @@ import Spotlight from "rc-spotlight";
 
 import "./index.css";
 
-const BackdropText = text => <h1 className="backdrop-text">{text}</h1>;
+const BackdropText = text => (
+  <h1 style={{ fontFamily: "Ubuntu" }} className="backdrop-text">
+    {text}
+  </h1>
+);
 
 const App = () => {
   const [backdropIndex, setBackdropIndex] = useState(0);
-  // useEffect(() => {
-  //   setInterval(function() {
-  //     setBackdropIndex(backdropIndex =>
-  //       backdropIndex === 4 ? 1 : backdropIndex + 1
-  //     );
-  //   }, 2000);
-  // }, []);
+  const [input, setInput] = useState();
+
+  useEffect(() => {
+    setInterval(function() {
+      setBackdropIndex(backdropIndex =>
+        backdropIndex === 4 ? 1 : backdropIndex + 1
+      );
+    }, 2000);
+  }, []);
 
   return (
-    <div style={{ backgroundColor: "#ff0000" }}>
+    <div className="container">
       <Spotlight
-        isActive={1 === 1}
+        isActive={1 === backdropIndex}
         renderOnBackdrop={BackdropText("This is title")}
       >
         <h1 className="header">Spotlight</h1>
       </Spotlight>
-      <Spotlight
-        isActive={1 === backdropIndex}
-        renderOnBackdrop={BackdropText("This is 2nd title")}
-      >
-        <h1 className="header">Spotlight 2</h1>
-      </Spotlight>
-      <Spotlight
-        isActive={1 === backdropIndex}
-        renderOnBackdrop={BackdropText("This is 3rd title")}
-      >
-        <h1 className="header">Spotlight 3</h1>
-      </Spotlight>
       <div>
         <Spotlight
-          isActive={2 === 2}
+          isActive={2 === backdropIndex}
           renderOnBackdrop={BackdropText("This is subtitle")}
+          inheritParentBackgroundColor
         >
-          <h2>You can NOT touch backdroped areas</h2>
-          <h2 className="header">
-            Sibling tag: You dont need to wrap it to one div
-          </h2>
+          <h2>You can NOT interact with DOMs under backdrop</h2>
         </Spotlight>
       </div>
       <Spotlight
         isActive={3 === backdropIndex}
         renderOnBackdrop={BackdropText("This is text input")}
       >
-        <input size="large" placeholder="Type something" />
+        <input
+          value={input}
+          style={{ fontSize: "24px", width: "100%" }}
+          size="large"
+          placeholder="Type something"
+          onChange={e => {
+            setInput(e.target.value);
+          }}
+        />
       </Spotlight>
     </div>
   );
