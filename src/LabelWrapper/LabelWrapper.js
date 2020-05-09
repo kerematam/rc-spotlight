984142d997da2efea8d7c2f98e17103f228abff1
Kerem <kerematam@gmail.com>
2020-05-09 17:05:21 +0300

    feat: support className prop for LabelWrapper

--- src/LabelWrapper/LabelWrapper.js
+++ src/LabelWrapper/LabelWrapper.js
@@ -3,20 +3,26 @@ import PropTypes from "prop-types";
 
 import styles from "./styles.css";
 
-const LabelWrapper = ({ children, style: styleProp, zIndex, center }) => {
-  const className = center
+const LabelWrapper = ({
+  children,
+  style: styleProp,
+  zIndex,
+  center,
+  className: classNameProp,
+  ...rest
+}) => {
+  let className = center
     ? styles.backdrop_content_center
     : styles.backdrop_content;
+  className += classNameProp ? ` ${classNameProp}` : "";
   const style = {
     ...(zIndex && { zIndex }),
     ...styleProp
   };
 
   return (
-    <div className={className}>
-      <div style={style}>
-        {children}
-      </div>
+    <div className={className} {...rest}>
+      <div style={style}>{children}</div>
     </div>
   );
 };
